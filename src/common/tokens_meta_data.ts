b39da72749a9d9f5557cba73e5f450fa301905db
icinsight <39129866+icinsight@users.noreply.github.com>
2019-06-16 15:28:57 -0300

    Alphabetize, added King and Link

--- src/common/tokens_meta_data.ts
+++ src/common/tokens_meta_data.ts
@@ -10,6 +10,70 @@ export interface TokenMetaData {
 
 export const KNOWN_TOKENS_META_DATA: TokenMetaData[] = [
     {
+        decimals: 8,
+        symbol: TokenSymbol.Xbc,
+        name: '0xBitcoin',
+        primaryColor: '#ff7423',
+        addresses: {
+            1: '0xb6ed7644c69416d67b522e20bc294a9a9b405b31',
+            4: '0x0000000000000000000000000000000000000000',
+            42: '0x0000000000000000000000000000000000000000',
+            50: '0x0000000000000000000000000000000000000000',
+        },
+    },
+
+    {
+        decimals: 18,
+        symbol: TokenSymbol.Dai,
+        name: 'DAI Stablecoin',
+        primaryColor: '#ffce6b',
+        addresses: {
+            1: '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359',
+            4: '0x0000000000000000000000000000000000000000',
+            42: '0x0000000000000000000000000000000000000000',
+            50: '0x0000000000000000000000000000000000000000',
+        },
+    },
+    {
+        decimals: 9,
+        symbol: TokenSymbol.Dgd,
+        name: 'DigixDao',
+        primaryColor: '#E1AA3E',
+        addresses: {
+            1: '0xE0B7927c4aF23765Cb51314A0E0521A9645F0E2A',
+            4: '0xc40a46ca4bc8e6057ed571e39cf400f3f935e4d5',
+            42: '0xA4f468c9c692eb6B4b8b06270dAe7A2CfeedcDe9',
+            50: '0xcdb594a32b1cc3479d8746279712c39d18a07fc0',
+        },
+    },
+
+	    {
+        decimals: 18,
+        symbol: TokenSymbol.King,
+        name: 'King Token',
+        primaryColor: '#cfb3e2',
+        addresses: {
+            1: '0xc46321EfE8883D22D81eD65CB4F0766e1004129d',
+            4: '0x0000000000000000000000000000000000000000',
+            42: '0x0000000000000000000000000000000000000000',
+            50: '0x0000000000000000000000000000000000000000',
+        },
+    },
+
+	    {
+        decimals: 18,
+        symbol: TokenSymbol.Link,
+        name: 'Chainlink',
+        primaryColor: '#6997e0',
+        addresses: {
+            1: '0x514910771af9ca656af840dff83e8264ecf986ca',
+            4: '0x0000000000000000000000000000000000000000',
+            42: '0x0000000000000000000000000000000000000000',
+            50: '0x0000000000000000000000000000000000000000',
+        },
+    },
+
+	{
         decimals: 18,
         symbol: TokenSymbol.Weth,
         name: 'Wrapped Ether',
@@ -33,40 +97,5 @@ export const KNOWN_TOKENS_META_DATA: TokenMetaData[] = [
             50: '0x871dd7c2b4b25e1aa18728e9d5f2af4c4e431f5c',
         },
     },
-    {
-        decimals: 18,
-        symbol: TokenSymbol.Dai,
-        name: 'DAI Stablecoin',
-        primaryColor: '#ffce6b',
-        addresses: {
-            1: '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359',
-            4: '0x0000000000000000000000000000000000000000',
-            42: '0x0000000000000000000000000000000000000000',
-            50: '0x0000000000000000000000000000000000000000',
-        },
-    },
-    {
-        decimals: 8,
-        symbol: TokenSymbol.Xbc,
-        name: '0xBitcoin',
-        primaryColor: '#ff7423',
-        addresses: {
-            1: '0xb6ed7644c69416d67b522e20bc294a9a9b405b31',
-            4: '0x0000000000000000000000000000000000000000',
-            42: '0x0000000000000000000000000000000000000000',
-            50: '0x0000000000000000000000000000000000000000',
-        },
-    },
-	    {
-        decimals: 9,
-        symbol: TokenSymbol.Dgd,
-        name: 'DigixDao',
-        primaryColor: '#E1AA3E',
-        addresses: {
-            1: '0xE0B7927c4aF23765Cb51314A0E0521A9645F0E2A',
-            4: '0xc40a46ca4bc8e6057ed571e39cf400f3f935e4d5',
-            42: '0xA4f468c9c692eb6B4b8b06270dAe7A2CfeedcDe9',
-            50: '0xcdb594a32b1cc3479d8746279712c39d18a07fc0',
-        },
-    },
+
 ];
